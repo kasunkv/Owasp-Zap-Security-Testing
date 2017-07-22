b8fe0bbda0aab85d9f97b5fe6e60212d9a87399c
Kasun Kodagoda <kvkrusl@gmail.com>
2017-07-22 11:56:45 +0530

    Add string constants to represent the Risk Codes

---
+++ OwaspZapSecurityTesting.Tests/RiskCode.cs
@@ -0,0 +1,10 @@
+﻿namespace OwaspZapSecurityTesting.Tests
+{
+    public class RiskCode
+    {
+        public const string Informational = "0";
+        public const string Low = "1";
+        public const string Medium = "2";
+        public const string High = "3";
+    }
+}
