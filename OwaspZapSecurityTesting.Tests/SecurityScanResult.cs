d7470f73e5dc551ec65c2734e36fb0ea47b2be36
Kasun Kodagoda <kvkrusl@gmail.com>
2017-07-22 11:57:15 +0530

    Added a class to store the count of security alerts.

---
+++ OwaspZapSecurityTesting.Tests/SecurityScanResult.cs
@@ -0,0 +1,10 @@
+﻿namespace OwaspZapSecurityTesting.Tests
+{
+    public class SecurityScanResult
+    {
+        public int InformationalAlerts { get; set; }
+        public int LowAlerts { get; set; }
+        public int MediumAlerts { get; set; }
+        public int HighAlerts { get; set; }
+    }
+}
